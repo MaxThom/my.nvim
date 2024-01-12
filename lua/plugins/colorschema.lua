8710f09c371bc0bc707f625c5638ad3121aecf5a
maxthom <maxthomassin@hotmail.com>
2024-01-13 09:51:16 +1300

    added unsync

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,14 +1,15 @@
 return {
+
+-- -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "tokionight-day",
+--     },
+--   },
+--
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-    },
-  },
 
   -- nord
   { "shaunsingh/nord.nvim" },
@@ -25,12 +26,12 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     opts = {
-      --     style = "moon",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      style = "day",
+      -- transparent = true,
+      -- styles = {
+      --   sidebars = "transparent",
+      --   floats = "transparent",
+      --},
     },
   },
 }
